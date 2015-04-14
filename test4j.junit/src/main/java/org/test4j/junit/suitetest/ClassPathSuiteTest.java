ac55b7e0cf8d48fe112a2af7599f56f1d917a31b
davey.wu <test4j@163.com>
2015-04-14 13:29:32 +0800

    rename java

--- test4j.junit/src/main/java/org/test4j/junit/suitetest/ClassPathSuiteTest.java
+++ test4j.junit/src/main/java/org/test4j/junit/suitetest/ClassPathSuiteTest.java
@@ -1,13 +1,13 @@
 package org.test4j.junit.suitetest;
 
 import org.junit.runner.RunWith;
-import org.test4j.junit.suitetest.suite.ClassPathSuite;
+import org.test4j.junit.suitetest.suite.ClassPathSuite1;
 
 /**
  * 运行classpath下的所有junit测试
  * 
  * @author darui.wudr
  */
-@RunWith(ClassPathSuite.class)
+@RunWith(ClassPathSuite1.class)
 public class ClassPathSuiteTest {
 }
