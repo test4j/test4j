550f9639bcf2c1df10131d353680a5fbb4746e53
davey.wu <test4j@163.com>
2015-04-14 11:17:46 +0800

    fix test

--- test4j.integrated/src/test/junit/IntegeratedTest.java
+++ test4j.integrated/src/test/junit/IntegeratedTest.java
@@ -1,5 +1,7 @@
+import org.test4j.junit.annotations.TestPath;
 import org.test4j.junit.suitetest.ClassPathSuiteTest;
 
+@TestPath(patterns = "org.test4j.module.*")
 public class IntegeratedTest extends ClassPathSuiteTest {
 
 }
