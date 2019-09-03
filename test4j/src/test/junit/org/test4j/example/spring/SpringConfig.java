ec0412bb4c8466b3cbbbb098c8cbdeea988cbac1
darui.wu <darui.wu@163.com>
2019-09-03 20:43:29 +0800

    fix test

--- test4j/src/test/junit/org/test4j/example/spring/SpringConfig.java
+++ test4j/src/test/junit/org/test4j/example/spring/SpringConfig.java
@@ -1,10 +1,17 @@
 package org.test4j.example.spring;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.test4j.module.database.sql.Test4JDataSourceHelper;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan(basePackages = "org.test4j.example.spring")
 public class SpringConfig {
-
+//    @Bean(name = "dataSource")
+//    public DataSource newDataSource() {
+//        return Test4JDataSourceHelper.createLocalDataSource("dataSource");
+//    }
 }
