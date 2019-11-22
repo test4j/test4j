09d3cfee1fddd3ad0efdce49578a4cb6a5d50368
darui.wu <darui.wu@163.com>
2019-11-22 22:58:52 +0800

    keyvalue

--- test4j-core/src/main/java/org/test4j/tools/datagen/KeyValue.java
+++ test4j-core/src/main/java/org/test4j/tools/datagen/KeyValue.java
@@ -1,12 +1,18 @@
 package org.test4j.tools.datagen;
 
-import org.test4j.module.ICore;
 import org.test4j.module.ICore.DataGenerator;
 
+import java.util.Random;
+import java.util.function.BiFunction;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static java.util.stream.Collectors.toList;
+
+/**
+ * @param <M>
+ * @author darui.wu
+ */
 public class KeyValue<M extends IDataMap> {
     private final M map;
 
@@ -19,6 +25,36 @@ public class KeyValue<M extends IDataMap> {
 
     public M values(Object value, Object... values) {
         this.map.put(this.column, value, values);
+        return this.map;
+    }
+
+    /**
+     * 将记录行号（从0开始）转换为赋值values数组序号（从0到length-1）<br/>
+     * o 如果转换后的数组序号小于0，按0处理<br/>
+     * o 如果转换后的数组序号大于数组size-1，按size-1处理
+     *
+     * @param changeIndex 
+     * @param values
+     * @return
+     */
+    public M values(Function<Integer, Integer> changeIndex, Object... values) {
+        if (values == null || values.length == 0) {
+            throw new RuntimeException("the values should be specified.");
+        }
+        DataGenerator generator = new DataGenerator() {
+            @Override
+            public Object generate(int index) {
+                Integer _index = changeIndex.apply(index);
+                if (_index == null || _index < 0) {
+                    _index = 0;
+                }
+                if (_index >= values.length) {
+                    _index = values.length - 1;
+                }
+                return values[_index];
+            }
+        };
+        this.map.put(this.column, generator);
         return this.map;
     }
 
@@ -83,7 +119,7 @@ public class KeyValue<M extends IDataMap> {
 
     /**
      * 按function生成内容，function入参index是以1开始以1步长递增
-     *ß
+     *
      * @param function
      * @return
      */
@@ -100,12 +136,97 @@ public class KeyValue<M extends IDataMap> {
      * @param values
      * @return
      */
-    public M functionObjs(Function<Object, Object> function, Object value, Object... values) {
+    public <O> M functionObjs(Function<O, Object> function, O value, O... values) {
         Object[] items = Stream.of(values)
                 .map(function::apply)
-                .collect(Collectors.toList())
+                .collect(toList())
                 .toArray(new Object[0]);
         this.map.put(this.column, function.apply(value), items);
+        return this.map;
+    }
+
+    /**
+     * 循环赋值
+     *
+     * @param loops
+     * @return
+     */
+    public M loop(Object... loops) {
+        this.map.put(this.column, DataGenerator.repeat(loops));
+        return this.map;
+    }
+
+    /**
+     * 以values数组作为入参，结合行序号生成需要的数据
+     * 需要在function里面自行处理（注意）序号越界的可能
+     *
+     * @param generator
+     * @param values
+     * @param <O>
+     * @return
+     */
+    public <O> M generateBy(BiFunction<Integer, O[], Object> generator, O... values) {
+        if (values == null || values.length == 0) {
+            throw new RuntimeException("the values should be specified.");
+        }
+        DataGenerator _generator = new DataGenerator() {
+            @Override
+            public Object generate(int index) {
+                return generator.apply(index, values);
+            }
+        };
+        this.map.put(this.column, _generator);
+        return this.map;
+    }
+
+    /**
+     * 根据记录行号生成数据
+     *
+     * @param generator
+     * @return
+     */
+    public M generate(Function<Integer, Object> generator) {
+        DataGenerator _generator = new DataGenerator() {
+            @Override
+            public Object generate(int index) {
+                return generator.apply(index);
+            }
+        };
+        this.map.put(this.column, _generator);
+        return this.map;
+    }
+
+    /**
+     * 随机生成
+     *
+     * @return
+     */
+    public M random() {
+        this.map.put(this.column, DataGenerator.random(this.column.getClass()));
+        return this.map;
+    }
+
+    private static Random random = new Random(System.currentTimeMillis());
+
+    /**
+     * 随机从randoms中挑选
+     *
+     * @param randoms
+     * @return
+     */
+    public M random(Object... randoms) {
+        if (randoms == null || randoms.length == 0) {
+            throw new RuntimeException("the randoms should be specified.");
+        }
+        DataGenerator generator = new DataGenerator() {
+            @Override
+            public Object generate(int index) {
+                int _index = random.nextInt() % randoms.length;
+                return randoms[_index];
+            }
+        };
+
+        this.map.put(this.column, generator);
         return this.map;
     }
 }
