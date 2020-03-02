45c4d532b0ee39fcd09d0dba66bc3d63dfc27f03
darui.wu <darui.wu@163.com>
2020-03-02 14:18:18 +0800

    add port

--- test4j-core/src/main/java/org/test4j/module/database/sql/DataSourceMariaDb4jCreator.java
+++ test4j-core/src/main/java/org/test4j/module/database/sql/DataSourceMariaDb4jCreator.java
@@ -4,12 +4,15 @@ import ch.vorburger.exec.ManagedProcessException;
 import ch.vorburger.mariadb4j.DB;
 import ch.vorburger.mariadb4j.DBConfigurationBuilder;
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.test4j.module.core.utility.MessageHelper;
 import org.test4j.tools.commons.ConfigHelper;
+import org.test4j.tools.commons.DateHelper;
 
 import javax.sql.DataSource;
 
 import static ch.vorburger.mariadb4j.DB.newEmbeddedDB;
 import static ch.vorburger.mariadb4j.DBConfigurationBuilder.newBuilder;
+import static org.test4j.module.core.utility.MessageHelper.warn;
 
 /**
  * MariaDb4j数据源创建
@@ -41,14 +44,19 @@ public class DataSourceMariaDb4jCreator {
         String username = DataSourceDefaultCreator.username(dataSourceName);
         String password = DataSourceDefaultCreator.password(dataSourceName);
         String driver = ConfigHelper.getString("dataSource.mariaDB4j.driver");
+        warn("begin to start MariaDB4j: " + DateHelper.currDateTimeStr());
+        int port = ConfigHelper.getInteger("dataSource.mariaDB4j.port",0);
 
-        DBConfigurationBuilder config = newBuilder().setPort(0);
+        DBConfigurationBuilder config = newBuilder().setPort(port);
         DB mariaDB = newEmbeddedDB(config.build());
         mariaDB.start();
+        String url = config.getURL(schema);
+        warn("MariaDB4j driver[ " + driver + " ], url[ " + url + " ], username[ " + username + " ], password[ " + password + " ].");
+        warn("end of start MariaDB4j:" + DateHelper.currDateTimeStr());
         BasicDataSource dataSource = new BasicDataSource();
         {
             dataSource.setDriverClassName(driver);
-            dataSource.setUrl(config.getURL(schema));
+            dataSource.setUrl(url);
             dataSource.setUsername(username);
             dataSource.setPassword(password);
         }
