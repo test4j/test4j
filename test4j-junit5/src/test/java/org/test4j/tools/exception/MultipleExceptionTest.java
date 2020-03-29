fd7bc33166725d54c39a9f19def8b60f5e3996a8
darui.wu <darui.wu@163.com>
2020-03-29 10:40:28 +0800

    add test

---
+++ test4j-junit5/src/test/java/org/test4j/tools/exception/MultipleExceptionTest.java
@@ -0,0 +1,45 @@
+package org.test4j.tools.exception;
+
+import java.util.Iterator;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.test4j.exception.MultipleException;
+import org.test4j.junit5.Test4J;
+import org.test4j.tools.datagen.DataProvider;
+
+@SuppressWarnings("rawtypes")
+public class MultipleExceptionTest extends Test4J {
+
+    @ParameterizedTest
+    @MethodSource("dataGetMultipleException")
+    public void testGetMultipleException(Throwable[] errors, boolean isNull) {
+        MultipleException exception = MultipleException.getMultipleException(errors);
+        want.bool(exception == null).is(isNull);
+    }
+
+    public static Iterator dataGetMultipleException() {
+        final Exception caused = new RuntimeException();
+        return new DataProvider() {
+            {
+                data(new Throwable[]{}, true);
+                data(new Throwable[]{null}, true);
+                data(new Throwable[]{null, null, null, null}, true);
+                data(new Throwable[]{caused}, false);
+                data(new Throwable[]{caused, null, null, null}, false);
+                data(new Throwable[]{null, caused, null, null}, false);
+                data(new Throwable[]{null, null, null, caused}, false);
+            }
+        };
+    }
+
+    @Test
+    public void testGetMultipleException() {
+        final Exception err1 = new RuntimeException("my error1");
+        final Exception err2 = new RuntimeException("my error2");
+        final Exception err3 = new RuntimeException("my error3");
+        MultipleException exception = MultipleException.getMultipleException(new Throwable[]{err1, err2, err3});
+        want.string(exception.getMessage()).containsInOrder("my error1", "my error2", "my error3");
+    }
+}
