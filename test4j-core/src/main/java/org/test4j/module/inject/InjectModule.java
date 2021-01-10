c40078fd9f113c47a3fa61866a2e5807c4ffc504
darui.wu <darui.wu@163.com>
2021-01-10 21:15:55 +0800

    delete unused

--- test4j-core/src/main/java/org/test4j/module/inject/InjectModule.java
+++ test4j-core/src/main/java/org/test4j/module/inject/InjectModule.java
@@ -1,45 +1,34 @@
 package org.test4j.module.inject;
 
-import org.test4j.module.core.Module;
-import org.test4j.module.core.internal.ModuleListener;
+import org.test4j.ITest4JListener;
 
 import java.lang.reflect.Method;
 
 import static org.test4j.module.inject.imposteriser.InjectHelper.injectIntoTestedObject;
 
-public class InjectModule implements Module {
-
+public class InjectModule implements ITest4JListener {
     @Override
-    public void init() {
-    }
-
-    @Override
-    public void afterInit() {
-    }
-
-    /**
-     * test4j扩展的注入<br>
-     *
-     * @param testedObject
-     */
-    private void injectInto(Object testedObject) {
-        injectIntoTestedObject(testedObject);
+    public void beforeAll(Class testClass) {
     }
 
     @Override
-    public ModuleListener getTestListener() {
-        return new InjectModuleListener();
+    public void afterAll() {
     }
 
-    protected class InjectModuleListener extends ModuleListener {
-        @Override
-        public void beforeExecute(Object testObject, Method testMethod) {
-            injectInto(testObject);
-        }
+    @Override
+    public void beforeMethod(Object target) {
+    }
 
-        @Override
-        protected String getName() {
-            return "InjectTestListener";
-        }
+    @Override
+    public void afterMethod() {
+    }
+
+    @Override
+    public void beforeExecute(Object testObject, Method testMethod) {
+        injectIntoTestedObject(testObject);
+    }
+
+    @Override
+    public void afterExecute(Object target, Method method, Throwable e) {
     }
 }
--- test4j-db-dep/src/main/java/PackageReadMe.java
+++
@@ -1,5 +0,0 @@
-/**
- * 只是一个数据库依赖项定义pom文件
- */
-public class PackageReadMe {
-}
